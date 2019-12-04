c3f89d124c7496ce9752d19975b031bc7aac4bff
YueHu105 <yh3218@columbia.edu>
2019-12-04 15:12:49 -0500

    templates

--- Tracker/views.py
+++ Tracker/views.py
@@ -33,7 +33,7 @@ class SquirrelAdd(CreateView):
     model = Squirrels
     fields = ["Latitude", "Longitude", "Unique_Squirrel_ID", "Shift", "Date", "Age", "Primary_Fur_Color", "Location", "Specific_Location", "Running", "Chasing", "Climbing", "Eating", "Foraging", "Other_Activities", "Kuks", "Quaas", "Moans", "Tail_flags", "Tail_twitches", "Approaches", "Indifferent", "Runs_from"]
     success_url = reverse_lazy('squirrel_list')
-    template_name = 'Tracker/squirrels_form.html'
+    template_name = 'Tracker/squirrels_add.html'
 
 class SquirrelStats(ListView):
     model = Squirrels
