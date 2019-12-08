4c1c35205dc03428f6c68813f6d449b72af3ae13
YueHu105 <yh3218@columbia.edu>
2019-12-07 22:02:37 -0500

    commit

--- Tracker/management/commands/import_squirrel_data.py
+++ Tracker/management/commands/import_squirrel_data.py
@@ -1,39 +1,52 @@
-from django.core.management.base import BaseCommand
-import pandas as pd
+# Todo 
+# Importation 
+import os 
+import csv 
+from django.core.management.base import BaseCommand 
 from Tracker.models import Squirrels
+import datetime as dt
+import pandas as pd
+import os
+
 
 class Command(BaseCommand):
-    help = 'help string'
+    help = 'import squirrel data csv'
 
     def add_arguments(self, parser):
-        parser.add_argument('file_path', type=str)
+        parser.add_argument('path')
 
-    def handle(self, *args, **options):
-        df = pd.read_csv(options['file_path'])
-        dfn = df.drop_duplicates('Unique_Squirrel_ID','first')
-        sqList = []
-        for _, row in dfn.iterrows():
-            sq = Squirrels(Latitude=row['Y'],
-                           Longitude=row['X'],
-                           Unique_Squirrel_ID=row['Unique_Squirrel_ID'],
-                           Shift=row['Shift'],
-                           Date=row['Date'],
-                           Age=row['Age'],
-                           Primary_Fur_Color=row['Primary_Fur_Color'],
-                           Location=row['Location'],
-                           Specific_Location=row['Specific_Location'],
-                           Running=row['Running'],
-                           Chasing=row['Chasing'],
-                           Climbing=row['Climbing'],
-                           Eating=row['Eating'], Foraging=row['Foraging'],
-                           Other_Activities=row['Other_Activities'],
-                           Kuks=row['Kuks'],
-                           Quaas=row['Quaas'],
-                           Moans=row['Moans'],
-                           Tail_flags=row['Tail_flags'],
-                           Tail_twitches=row['Tail_twitches'],
-                           Approaches=row['Approaches'],
-                           Indifferent=row['Indifferent'],
-                           Runs_from=row['Runs_from'])
-            sqList.append(sq)
-        Squirrels.objects.bulk_create(sqList)
+    def handle(self,*args, **options):
+         path = options['path']
+         df = pd.read_csv(path)
+         df_new = df.drop_duplicates('Unique_Squirrel_ID', 'first')
+         df_new.to_csv("squirrel_data", index=False)
+         with open("squirrel_data",'r') as file:
+              data = csv.reader(file, delimiter=',')
+              next(data,None)
+              for row in data:
+                   squirrel,created = Squirrels.objects.get_or_create(
+                        Longitude = row[0],
+                        Latitude = row[1],
+                        Unique_Squirrel_ID = row[2],
+                        Date = dt.datetime.strptime(row[5].strip(),'%m%d%Y').date(),
+                        Age = row[7],
+                        Shift = row[4],
+                        Primary_Fur_Color = row[8],
+                        Location = row[12],
+                        Specific_Location = row[14],
+                        Other_Activities = row[20],
+                        Running = True if "true" in row[15].lower() else False,
+                        Chasing = True if "true" in row[16].lower() else False,
+                        Climbing = True if "true" in row[17].lower() else False,
+                        Eating = True if "true" in row[18].lower() else False,
+                        Foraging = True if "true" in row[19].lower() else False,
+                        Kuks = True if "true" in row[21].lower() else False,
+                        Quaas = True if "true" in row[22].lower() else False,
+                        Moans = True if "true" in row[23].lower() else False,
+                        Tail_flags = True if "true" in row[24].lower() else False,
+                        Tail_twitches = True if "true" in row[25].lower() else False,
+                        Approaches = True if "true" in row[26].lower() else False,
+                        Indifferent = True if "true" in row[27].lower() else False,
+                        Runs_from = True if "true" in row[28].lower() else False,)
+                   if created:
+                        squirrel.save()
